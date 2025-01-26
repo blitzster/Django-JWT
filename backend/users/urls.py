f27a056e614982a9bc24b4d11745a95538b7ac6b
Akshay <blitz122333@gmail.com>
2025-01-26 23:20:43 +0530

    first commit

---
+++ backend/users/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import UserInfoView, UserRegistrationView
+
+
+
+urlpatterns = [
+    path("user-info/", UserInfoView.as_view(), name="user-info"),
+    path('register/', UserRegistrationView.as_view(), name="register-user")
+]
