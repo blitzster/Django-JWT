6c6b928c9ed24b8c0ed3175461a1001492f56509
Akshay <blitz122333@gmail.com>
2025-01-26 23:44:23 +0530

    cookie is set and token generated for login

--- backend/api/settings.py
+++ backend/api/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'users',
     'rest_framework',
+    'rest_framework_simplejwt',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +125,12 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'users.CustomUser'
+
+
+REST_FRAMEWORK={
+    'DEFAULT_AUTHENTICATION_CLASSES':(
+
+        'rest_framework_simplejwt.authentication.CookieJWTAuthentication',
+    )
+
+}
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from rest_framework.generics import RetrieveUpdateAPIView, CreateAPIView
 from rest_framework.permissions import IsAuthenticated
-from .serializers import CustomUserSerializer, RegisterUserSerializer
+from .serializers import CustomUserSerializer, RegisterUserSerializer, LoginUserSerializer
+from rest_framework.views import APIView
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.response import Response
+from rest_framework import status
 
 class UserInfoView(RetrieveUpdateAPIView):
     permission_classes = (IsAuthenticated,)
@@ -12,3 +16,34 @@ class UserInfoView(RetrieveUpdateAPIView):
 
 class UserRegistrationView(CreateAPIView):
     serializer_class = RegisterUserSerializer
+
+
+class LoginView(APIView):
+    def post(self, request):
+        serializer = LoginUserSerializer(data=request.data)
+
+        #generate tokens
+        if serializer.is_valid():
+            user = serializer.validated_data
+            refresh = RefreshToken.for_user(user)
+            access_token = str(refresh.access_token)
+
+            response = Response({'user':CustomUserSerializer(user).data},
+                                status=status.HTTP_200_OK)
+            
+            response.set_cookie(key="access_token", value=access_token,
+                                httponly=True,
+                                secure=True,
+                                samesite="Strict")
+            
+            response.set_cookie(key="refresh_token",
+                                value=str(refresh),
+                                httponly=True,
+                                secure=True,
+                                samesite="Strict")
+            return response
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+class LogoutView():
+    pass
